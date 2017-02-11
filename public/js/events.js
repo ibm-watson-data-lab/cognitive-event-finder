2dd1dd5664cdf9eac781fc7c71a69753a9d44e65
Ryan Baumann <rsbaumann@gmail.com>
2017-02-10 17:55:40 -0800

    updated text link map

--- public/js/events.js
+++ public/js/events.js
@@ -13,7 +13,7 @@ var app = new Vue({
             var feature;
             var min = [];
             var max = [];
-            for (var i=0; i<events.length; i++) {
+            for (var i = 0; i < events.length; i++) {
                 point = events[i];
                 feature = { type: "Feature" };
                 feature.properties = {};
@@ -27,7 +27,7 @@ var app = new Vue({
                                 feature.geometry.type = point[prop].type;
                             }
                             // something is screwy with data so do this weird hack
-                            if ( point.geometry.coordinates[0]<0) // 0 is x
+                            if (point.geometry.coordinates[0] < 0) // 0 is x
                                 feature.geometry.coordinates = point.geometry.coordinates;
                             else
                                 feature.geometry.coordinates = [point.geometry.coordinates[1], point.geometry.coordinates[0]];
@@ -39,14 +39,14 @@ var app = new Vue({
 
                 x = feature.geometry.coordinates[0];
                 y = feature.geometry.coordinates[1];
-                if (i==0) { // set initial bbox
+                if (i == 0) { // set initial bbox
                     min[0] = max[0] = x;
                     min[1] = max[1] = y;
                 } else {
-                    if (x<min[0]) min[0] = x;
-                    if (x>max[0]) max[0] = x;
-                    if (y<min[1]) min[1] = y;
-                    if (y>max[1]) max[1] = y;
+                    if (x < min[0]) min[0] = x;
+                    if (x > max[0]) max[0] = x;
+                    if (y < min[1]) min[1] = y;
+                    if (y > max[1]) max[1] = y;
                 }
 
                 features.push(feature);
@@ -55,44 +55,57 @@ var app = new Vue({
             // console.log(geoj);
 
             document.getElementById('map').setAttribute("style", "display:inline");
-            var popup = new mapboxgl.Popup({closeButton: false,closeOnClick: true});
+            var popup = new mapboxgl.Popup({ closeButton: false, closeOnClick: true });
             var map = new mapboxgl.Map({
                 container: "map",
-                style: "mapbox://styles/mapbox/light-v9",
-                center: [ -97.74306, 30.26715],
-                zoom: 12
+                style: "mapbox://styles/mapbox/streets-v9",
+                center: [-97.74306, 30.26715],
+                zoom: 15,
+                pitch: 30
             });
-            map.fitBounds([min, max], {"padding":12});
+            try {
+                map.fitBounds([min, max], { "padding": 12 });
+            } catch (e) {
+                console.log(e)
+            }
 
-            map.on('load', function(){
+            map.addControl(new MapboxDirections({
+                accessToken: mapboxgl.accessToken,
+                unit: 'metric',
+                profile: 'walking',
+                geocoder: {
+                    bbox: [[-98, 29],[-97, 31]]
+                }
+            }), 'top-left');
+
+            map.on('load', function() {
                 map.addLayer({
-                    "id":"eventslayer",
-                    "type":"circle",
+                    "id": "eventslayer",
+                    "type": "circle",
                     "source": {
-                        "type":"geojson",
+                        "type": "geojson",
                         "cluster": true,
                         "clusterMaxZoom": 11,
                         "clusterRadius": 20,
-                        "data":geoj
+                        "data": geoj
                     },
                     "paint": {
-                        "circle-radius":8,
-                        "circle-color":"#ff0000"
+                        "circle-radius": 8,
+                        "circle-color": "#ff0000"
                     }
                 });
             });
 
             map.on('mousemove', function(e) {
-                var fs = map.queryRenderedFeatures(e.point,{layers:["eventslayer"]});
-                map.getCanvas().style.cursor=(fs.length)?"pointer":"";
-                if (!fs.length) {popup.remove();return;};
+                var fs = map.queryRenderedFeatures(e.point, { layers: ["eventslayer"] });
+                map.getCanvas().style.cursor = (fs.length) ? "pointer" : "";
+                if (!fs.length) {
+                    popup.remove();
+                    return;
+                };
                 var f = fs[0];
-                // var keylength = Object.keys(f.properties).length;
-                // popuphtml = "";
-                // for (var key in f.properties) {
-                //     popuphtml += "<b>"+key+": </b> "+f.properties[key]+"<br/>"
-                // }
-                popuphtml = "<b>"+f.properties.name+"</b><p>"+f.properties.description+"</p>";
+
+                popuphtml = "<b>" + f.properties.name + "</b><p>" + f.properties.description + "</p>";
                 popup.setLngLat(f.geometry.coordinates).setHTML(popuphtml).addTo(map);
             });
         }
